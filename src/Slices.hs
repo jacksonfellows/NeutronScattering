d384d48cf2d19a99f3e2cbf6b43d69add9c88096
Jackson Fellows <jackson@fellowsfamily.com>
2019-02-21 15:34:04 -0800

    no more fized view port

--- src/Slices.hs
+++ src/Slices.hs
@@ -14,10 +14,10 @@ module Slices
 
 import           Codec.Picture
 import           Codec.Picture.Types
-import           Control.Arrow               (first)
 import           Control.Monad               (unless, when)
 import           Control.Monad.ST
 import           Control.Monad.Trans.Class   (lift)
+import           Data.Bifunctor              (first)
 import           Data.Maybe                  (fromJust)
 import qualified Data.Vector.Storable        as V
 import           Data.Word
@@ -27,6 +27,7 @@ import qualified Graphics.GPipe.Context.GLFW as GLFW
 import           Text.Printf                 (printf)
 
 import           AABB
+import           Ray
 
 type Val = Pixel8
 type Adder s = V3 Double -> Val -> ST s ()
@@ -67,6 +68,9 @@ writeSlices slices = mapM_
 instance Epsilon VFloat where
     nearZero _ = False
 
+instance Epsilon FFloat where
+    nearZero _ = False
+
 display :: IO ()
 display = runContextT GLFW.defaultHandleConfig $ do
     win <- newWindow (WindowFormatColor RGB8) (GLFW.defaultWindowConfig "Hello World!")
@@ -101,30 +105,45 @@ display = runContextT GLFW.defaultHandleConfig $ do
                               , 3, 2, 6
                               , 6, 7, 3 ]
 
-    uniformBuffer :: Buffer os (Uniform (B3 Float)) <- newBuffer 1
+    let makePrimitives = do
+            vertexArray <- newVertexArray vertexBuffer
+            indexArray <- newIndexArray indexBuffer Nothing
+            return $ toPrimitiveArrayIndexed TriangleList indexArray vertexArray
+
+    uniform :: Buffer os (Uniform (V4 (B4 Float))) <- newBuffer 1
 
     shader <- compileShader $ do
-        primitiveStream <- toPrimitiveStream id
-        camPos <- getUniform (const (uniformBuffer,0))
-        let viewMat = lookAt camPos (V3 0 0 0) (V3 0 1 0)
-            projMat = perspective (pi/3) 1 1 100
-            primitiveStream' = fmap (first (projMat !*! viewMat !*)) primitiveStream
-        fragmentStream <- rasterize (const (Front, ViewPort (V2 0 0) (V2 500 500), DepthRange 0 1)) primitiveStream'
-        drawWindowColor (const (win, ContextColorOption NoBlending (V3 True True True))) fragmentStream
+        primitiveStream <- toPrimitiveStream primitives
+        modelViewProj <- getUniform (const (uniform,0))
+        let primitiveStream' = fmap (first (modelViewProj !*)) primitiveStream
+        frontFrags :: FragmentStream (V3 FFloat) <- rasterize rasterOptions primitiveStream'
+        drawWindowColor (const (win, ContextColorOption NoBlending (pure True))) frontFrags
 
-    loop vertexBuffer indexBuffer shader win uniformBuffer
+    loop win shader makePrimitives uniform
 
-loop vertexBuffer indexBuffer shader win uniformBuffer = do
+loop win shader makePrimitives uniform = do
     t <- lift GLFW.getTime >>= return . double2Float . fromJust
-    writeBuffer uniformBuffer 0 [V3 ((sin t) * 10) 5 ((cos t) * 10)]
+    Just (w,h) <- GLFW.getWindowSize win
+    let camPos = V3 ((sin t) * 10) 5 ((cos t) * 10)
+        center = V3 0 0 0
+        up = V3 0 1 0
+        viewMat = lookAt camPos center up
+        fov = pi/3
+        projMat = perspective fov (fromIntegral w / fromIntegral h) 1 100
+        viewProjMat = projMat !*! viewMat
+    writeBuffer uniform 0 [viewProjMat]
+
     render $ do
-        clearWindowColor win (V3 0 0 0)
-        vertexArray <- newVertexArray vertexBuffer
-        indexArray <- newIndexArray indexBuffer Nothing
-        let primitiveArray = toPrimitiveArrayIndexed TriangleList indexArray vertexArray
-        shader primitiveArray
+        clearWindowColor win 0
+        prims <- makePrimitives
+        shader $ ShaderEnvironment prims (Front, ViewPort 0 (V2 w h), DepthRange 0 1)
     swapWindowBuffers win
 
     closeRequested <- GLFW.windowShouldClose win
     unless (closeRequested == Just True) $
-        loop vertexBuffer indexBuffer shader win uniformBuffer
+        loop win shader makePrimitives uniform
+
+data ShaderEnvironment = ShaderEnvironment
+    { primitives    :: PrimitiveArray Triangles (B4 Float, B3 Float)
+    , rasterOptions :: (Side, ViewPort, DepthRange)
+    }
